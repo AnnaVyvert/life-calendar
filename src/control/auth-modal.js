62e73d5c4dd291e14d955795f0e64aeecfcdd465
AnnaVyvert <0073av@gmail.com>
2024-09-16 19:35:04 +0800

    fix: 🐛 validation for registration, update todo

--- src/control/auth-modal.js
+++ src/control/auth-modal.js
@@ -1,6 +1,18 @@
+const maxAgeToRegister = 75;
+
 function registerUser() {
-  if (!isValidDate(new Date(birthdayInputElement.value))) {
-    birthdayInputElement.value = '';
+  let userBirthday = new Date(birthdayInputElement.value);
+  if (!isValidDate(userBirthday)) {
+    birthdayValidationElement.innerHTML = `the given date is invalid.`
+    return;
+  }
+  else if (new Date().getFullYear() - userBirthday.getFullYear() > maxAgeToRegister) {
+    birthdayValidationElement.innerHTML = `it's hard to use this site<br>if you are older than ${maxAgeToRegister} y.o. =)`
+    return;
+  }
+  else if (new Date().getTime() < userBirthday.getTime()) {
+    birthdayValidationElement.innerHTML = `how can you write this?<br>child of future... =)`
+    return;
   }
 
   createUser(nameInputElement.value, birthdayInputElement.value)
--- src/view/auth-modal.js
+++ src/view/auth-modal.js
@@ -4,3 +4,4 @@ const authFormElement = document.querySelector('dialog.login form');
 
 const nameInputElement = document.querySelector('dialog.login input[name="name"]');
 const birthdayInputElement = document.querySelector('dialog.login input[name="birthday"]');
+const birthdayValidationElement = document.querySelector('dialog.login .date-validation');
